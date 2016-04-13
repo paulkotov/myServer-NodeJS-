f14fe25f434869ed7aceac51d937c373f4d06351
Paul Kotov <paulkotov1@gmail.com>
2016-04-14 00:43:11 +0600

    added basic config

---
+++ app.js
@@ -0,0 +1,23 @@
+var config = require("nconf");
+var express = require('express');
+var http = require('http');
+var app = express();
+
+config.argv()
+    .env()
+    .file({ file: 'config.json' });
+
+//boot
+require('./boot/index')(app);
+
+// routing
+require('./routes/index')(app);
+
+//mysql
+require('./mysql/mysql')(app);
+
+http.createServer(app).listen(app.get('port'), function () {
+    if ('development' == app.get('env')) {
+        console.log('Express server listening on port ' + app.get('port'));
+    }
+});
